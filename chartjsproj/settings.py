8011c17d46e9836f3fe293f401421f680af70f76
duaneywadey <idrdequito.cvt@eac.edu.ph>
2023-02-10 14:21:28 +0800

    edit settings

--- chartjsproj/settings.py
+++ chartjsproj/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
+STATIC_ROOT = "/var/www/example.com/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
